7000f8c23633da72fb385be7e6c6fc67519929fc
Andrew Walker <walker.ab@gmail.com>
2014-07-21 21:06:14 +1000

    adding package skeleton

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name         = 'dtmc',
+    version      = '0.0.1',
+    description  = 'Discrete Time Markov Chain analysis and simulation',
+    author       = 'Andrew Walker',
+    author_email = 'walker.ab@gmail.com',
+    packages     = find_packages(),
+    classifiers  = [
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ]
+)
