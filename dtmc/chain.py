28cc0a274717d3062c9ee8ac15225695005d5672
Andrew Walker <walker.ab@gmail.com>
2014-07-21 21:56:41 +1000

    adding irreducible

--- dtmc/chain.py
+++ dtmc/chain.py
@@ -1,4 +1,14 @@
 import numpy
+import networkx
+
+class DTMCError(Exception):
+    """Serious DTMC Error"""
+
+def is_square(P):
+    return P.shape[0] == P.shape[1]
+
+def is_right_stochastic(P):
+    return numpy.count_nonzero(numpy.sum(P, axis = 1) - 1.0) == 0
 
 class DiscreteTimeMarkovChain(object):
     """A discrete time Markov chain
@@ -9,9 +19,25 @@ class DiscreteTimeMarkovChain(object):
 
         Parameters
         ----------
-        transition_matrix : matrix
+        transition_matrix : matrix-like
             Data that describes the transitions between states in the
-            chain. 
+            chain. The rows of the transition matrix need to sum to one
         """
-        self._P = transition_matrix
+        # TODO - none of the work precludes the use of sparse 
+        # representations, but as a first guess, this is going to
+        # simplify the code signficantly
+        self._P = numpy.asmatrix( transition_matrix )
+        if not is_square(self._P):
+            raise DTMCError('Non-square transition matrix')
+
+        if not is_right_stochastic(self._P):
+            raise DTMCError('Transition Matrix is not right stochastic')
+
+    def _as_digraph(self):
+        return networkx.DiGraph(self._P)
+
+    def irreducible(self):
+        G = self._as_digraph()
+        return networkx.number_strongly_connected_components(G) == 1
+
 
--- dtmc/tests/test_chain.py
+++ dtmc/tests/test_chain.py
@@ -4,4 +4,17 @@ import dtmc
 class DiscreteTimeMarkovChainTest(unittest.TestCase):
 
     def test_trivial(self):
-        chain = dtmc.DiscreteTimeMarkovChain([[0.]])
+        chain = dtmc.DiscreteTimeMarkovChain([[1.]])
+        self.assertTrue(chain is not None)
+
+    def test_non_square_input(self):
+        with self.assertRaises(dtmc.DTMCError): 
+            dtmc.DiscreteTimeMarkovChain([[1., 0.]])
+
+    def test_non_stochastic_matrix(self):
+        with self.assertRaises(dtmc.DTMCError): 
+            dtmc.DiscreteTimeMarkovChain([[0.]])
+
+    def test_trivially_irreducible(self):
+        chain = dtmc.DiscreteTimeMarkovChain([[1.]])
+        self.assertTrue(chain.irreducible())
