e5c31bec2733a437833d61176b6d180c229ebcc0
Andrew Walker <walker.ab@gmail.com>
2014-07-24 10:05:08 +1000

    fixed stationary_distribution signature

--- dtmc/chain.py
+++ dtmc/chain.py
@@ -1,5 +1,5 @@
 import numpy
-import networkx
+import networkx as nx
 
 class DTMCError(Exception):
     """Serious DTMC Error"""
@@ -32,7 +32,7 @@ def vertex_out_component(G, nodes):
     """
     s = set(nodes)
     for n in nodes:
-        reachable = networkx.shortest_path_length(G, n).keys()
+        reachable = nx.shortest_path_length(G, n).keys()
         s = s.union(reachable)
     return s
 
@@ -71,7 +71,7 @@ class DiscreteTimeMarkovChain(object):
             raise DTMCError('Transition Matrix is not right stochastic')
 
     def _as_digraph(self):
-        return networkx.DiGraph(self._P)
+        return nx.DiGraph(self._P)
 
     def _as_dense(self):
         return self._P
@@ -81,20 +81,16 @@ class DiscreteTimeMarkovChain(object):
 
     def irreducible(self):
         G = self._as_digraph()
-        return networkx.number_strongly_connected_components(G) == 1
+        return nx.number_strongly_connected_components(G) == 1
 
     def aperiodic(self):
         G = self._as_digraph()
-        return networkx.is_aperiodic(G)
+        return nx.is_aperiodic(G)
 
     def stationary_distribution(self):
         # in MATLAB, this is simply A\b
         # in Python, things are a little uglier, because numpy.linalg.solve
         #   only deals with square matrices.
-        
-        P = self._as_dense()
-
-    def stationary_distribution(self):
         P = self._P
         n = P.shape[0]
         I = numpy.eye(n)
@@ -111,9 +107,10 @@ class DiscreteTimeMarkovChain(object):
 
         return numpy.linalg.lstsq(A, b)[0]
 
-    def absorbing(self, i, eps = 1e-10):
-        # TODO - is this the right way to do this?
-        return (self._P[i, i] - 1.0) < eps
+    def absorbing(self, i):
+        G = self._as_digraph()
+        
+        return numpy.sum(self._P[i, :]) == self._P[i, i] 
 
     def absorbing_states(self):
         """Return a list of states that are absorbing
@@ -123,17 +120,73 @@ class DiscreteTimeMarkovChain(object):
         return [ i for i in xrange(self._num_states()) if self.absorbing(i) ]
 
     def is_absorbing(self):
-        # TODO - This is a TDD placeholder
-        return len(self.absorbing_states()) > 0
+        # to be absorbing all states must eventually reach an
+        # absorbing state
+        G = self._as_digraph()
+        for cpt in nx.weakly_connected_components(G):
+            Gprime = nx.subgraph(G, cpt)
+            Gprime.remove_edges_from(Gprime.selfloop_edges())
+            akeys = [k for k, v in Gprime.out_degree().iteritems() if v == 0]
+            if len(akeys) == 0:
+                return False
+        return True
 
     def communicating_classes(self):
         G = self._as_digraph()
-        return list(networkx.strongly_connected_components(G))
+        return list(nx.strongly_connected_components(G))
 
     def recurrent_classes(self):
-        return [ recurrent_class(cpt) for cpt in self.communicating_classes() ]
+        G = self._as_digraph()
+        classes = self.communicating_classes()
+        return [ cpt for cpt in classes if recurrent_class(G, cpt) ]
 
     def transient_classes(self):
-        return [ transient_class(cpt) for cpt in self.communicating_classes() ]
+        G = self._as_digraph()
+        classes = self.communicating_classes()
+        return [ cpt for cpt in classes if transient_class(G, cpt) ]
 
+    def transient_states(self):
+        return sum(self.transient_classes(), [])
 
+    def recurrent_states(self):
+        return sum(self.recurrent_classes(), [])
+
+    def is_canonical(self):
+        """Check if states are in canonical order
+
+        True iff the transient states are before the recurrent states
+        """
+        return max(self.transient_states()) < min(self.recurrent_states())
+
+    def canonical_permutation(self):
+        if self.is_canonical():
+            # attempt to be stable, ie/ do not change the
+            # order of states unless you need to
+            return range(self.num_states())
+
+        lst = []
+        for cls in self.transient_classes():
+            lst += cls
+        for cls in self.recurrent_classes():
+            lst += cls
+        return lst
+
+    def permute(self, p):
+        M = self._P.copy()
+        M = M[p, :]
+        M = M[:, p]
+        return DiscreteTimeMarkovChain(M)
+
+    def canonical_form(self):
+        p = self.canonical_permutation()
+        return self.permute(p), p
+
+    def Qmatrix(self):
+        alt, p = self.canonical_form()
+        transient = alt.transient_states()
+        Q = alt._P[transient, :]
+        Q = Q[:, transient]
+        return Q, p[transient]
+
+    def fundamental_matrix(self):
+        return None
